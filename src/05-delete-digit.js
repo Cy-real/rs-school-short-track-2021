6d83fe8923045fcaeb0822f534583bafd7ba2a50
Cy-real <79482914+Cy-real@users.noreply.github.com>
2021-04-13 01:45:59 +0300

    feat: add solution

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -10,7 +10,20 @@
  *
  */
 function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+  let str = n.toString().split('');
+  let min = str[0];
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] < min) min = str[i];
+  }
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] === min) {
+      str.splice(j, 1);
+      break;
+    }
+  }
+  str = str.join('');
+  const number = Number(str);
+  return number;
 }
 
 module.exports = deleteDigit;
