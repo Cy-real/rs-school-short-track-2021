4acb613acc0b74c3aec6e4097fabe95307df059d
Cy-real <79482914+Cy-real@users.noreply.github.com>
2021-04-13 00:36:55 +0300

    feat: add solution

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -10,7 +10,19 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+  const s3 = s1.split('');
+  const s4 = s2.split('');
+  let number = 0;
+  for (let i = 0; i < s3.length; i++) {
+    for (let j = 0; j < s4.length; j++) {
+      if (s3[i] === s4[j]) {
+        number++;
+        s4.splice(j, 1);
+        break;
+      }
+    }
+  }
+  return number;
 }
 
 module.exports = getCommonCharacterCount;
